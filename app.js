3edbf11c99ee1542d08a27d43481bb4ec6602c9e
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2020-04-17 14:59:59 -0500

    seventh commit

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 dotenv.config({ path: '.env' })
+
+dev_db_url = process.env.ATLAS_URI
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
